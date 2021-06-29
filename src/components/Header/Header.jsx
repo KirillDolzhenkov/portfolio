6d7b0942f1c385d8a73b3e293d8a726095f6f3b1
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2021-06-29 23:01:11 +0300

    created project without typescript + created Header & Nav components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Header.module.css"
+import {Nav} from "../Navbar/Nav";
+
+const Header = () => {
+    return(
+        <div className={styles.header}>
+            <Nav/>
+        </div>
+    )
+}
+
+export {
+    Header
+}
