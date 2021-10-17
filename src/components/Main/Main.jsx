90cb33fe3a246dd661e87c7e433af065b77156c9
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2021-10-17 23:54:25 +0300

    created common container style

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import styles from "./Main.module.css"
+import style from "./Main.module.css"
+import styleContainer from "../../common/styles/Container.module.css"
 
 const Main = () => {
+
     return (
-        <div className={styles.mainBlock}>
-            <div className={styles.container}>
-                <div className={styles.text}>
+        <div className={style.mainBlock}>
+            <div className={styleContainer.container}>
+                <div className={style.text}>
                     <span>Hi there</span>
                     <h1>I am Kirill Dolzhenkov</h1>
                     <p>Frontend Developer</p>
                 </div>
-                <div className={styles.photo}></div>
+                <div className={style.photo}></div>
             </div>
         </div>
-    )
+)
 }
 export {
     Main
