740099cd828185b0f7e789e31a9d00e51600e38b
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2021-10-28 23:41:16 +0300

    added skills block

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import "./App.css";
 import {Header} from "./components/Header/Header";
 import {Main} from "./components/Main/Main";
+import {Skills} from "./components/Skills/Skills";
 
 const App = () => {
   return (
     <div className="App">
         <Header/>
         <Main/>
+        <Skills/>
     </div>
   );
 }
