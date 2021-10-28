be97318181cb11cec3f0374abcf921b765a40ae1
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2021-10-28 23:59:53 +0300

    added skill

---
+++ src/components/Skills/Skill/Skill.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import style from "./Skill.module.css"
+
+
+const Skill = () => {
+    return (
+        <div className={style.skill}>
+
+        </div>
+    )
+}
+
+export {
+    Skill
+}
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -2,17 +2,21 @@ import React from "react"
 
 import style from "./Skills.module.css"
 import styleContainer from "../../common/styles/Container.module.css";
+import {Skill} from "./Skill/Skill";
 
 const Skills = () => {
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <h2 className={style.title}>Skills</h2>
-                <div className={style.skills}></div>
+                <div className={style.skills}>
+                    <Skill/>
+                </div>
             </div>
         </div>
     )
 }
+
 export {
     Skills
 }
